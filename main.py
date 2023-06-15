41676d7e28f3019906f04f0b1c1d885efb0ff454
OtisBaker4 <136388873+OtisBaker4@users.noreply.github.com>
2023-06-15 21:24:50 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root=Tk()
+
+root.title('Metronomical By Otis Baker')
+
+root.geometry('1270x685')
+
+root.iconbitmap('icon.ico')
+
+headingLabel=Label(root,text='Metronomical',font=('times new roman',30,'bold'),bg='gray20',fg='gold')
+
+headingLabel.pack()
+
+root.mainloop()
